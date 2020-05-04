fef77a8efb7bb207be5ed12a0e34aa84aaa94291
Paweł Waligóra <pawelwaligora1337@gmail.com>
2020-05-04 22:07:03 +0200

    add router to client project

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Chat from './components/Chat/Chat';
+import Join from './components/Join/Join';
 
-function App() {
-	return <div className="App">Chatf</div>;
-}
+const App = () => (
+	<Router>
+		<Route path="/" exact component={Join} />
+		<Route path="/chat" component={Chat} />
+	</Router>
+);
 
 export default App;

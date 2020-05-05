4a95029864fbad0379503d567cbcafa47da28902
Paweł Waligóra <pawelwaligora1337@gmail.com>
2020-05-05 20:36:39 +0200

    add socket connection between client and server

--- client/src/components/Chat/Chat.tsx
+++ client/src/components/Chat/Chat.tsx
@@ -1,5 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
 
-const Chat = () => <div>Chat</div>;
+let socket;
+
+const Chat: React.FC = ({ location }) => {
+	const [ name, setName ] = useState<string>('');
+	const [ room, setRoom ] = useState<string>('');
+
+	useEffect(() => {
+		const { data, name } = queryString.parse(location.search);
+
+		socket = io('localhost:5000');
+
+		setName(name);
+		setRoom(room);
+	});
+	return <div>Chat app</div>;
+};
 
 export default Chat;

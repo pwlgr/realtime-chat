49898cb8a9353485ded8aa6d8e1c5ad924cf09d9
Paweł Waligóra <pawelwaligora1337@gmail.com>
2020-05-05 20:09:43 +0200

    add basic styles and ui for join component

--- client/src/components/Join/Join.tsx
+++ client/src/components/Join/Join.tsx
@@ -1,5 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Join.css';
 
-const Join = () => <div>Join</div>;
+const Join: React.FC = () => {
+	const [ name, setName ] = useState<string>('');
+	const [ room, setRoom ] = useState<string>('');
+	return (
+		<div className="joinOuterContainer">
+			<div className="joinInnerContainer">
+				<h1 className="heading">Join</h1>
+				<div>
+					<input
+						placeholder="Name"
+						className="joinInput"
+						type="text"
+						onChange={(e) => setName(e.target.value)}
+					/>
+				</div>
+				<div>
+					<input
+						placeholder="Room"
+						className="joinInput mt-20"
+						type="text"
+						onChange={(e) => setRoom(e.target.value)}
+					/>
+				</div>
+				<Link
+					onClick={(e) => (!name || !room ? e.preventDefault() : null)}
+					to={`/chat?name=${name}&room=${room}`}
+				>
+					<button className={'button mt-20'} type="submit">
+						Sign In
+					</button>
+				</Link>
+			</div>
+		</div>
+	);
+};
 
 export default Join;
--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
+const router = require('./router');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+io.on('connection', (socket) => {
+	console.log('socket connection');
+	socket.on('disconnect', () => {
+		console.log('socket disconnected');
+	});
+});
+
+app.use(router);
 
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));

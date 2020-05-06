0ce6b3c87391e7fae98ce21999ed62453d687deb
Paweł Waligóra <pawelwaligora1337@gmail.com>
2020-05-06 22:27:05 +0200

    disconnect socket when chat component is unmounted

--- client/src/components/Chat/Chat.tsx
+++ client/src/components/Chat/Chat.tsx
@@ -19,6 +19,10 @@ const Chat: React.FC = ({ location }) => {
 			setRoom(room);
 
 			socket.emit('join', { name, room }, () => {});
+			return () => {
+				socket.emit('disconnect');
+				socket.off();
+			};
 		},
 		[ ENDPOINT, location.search ]
 	);

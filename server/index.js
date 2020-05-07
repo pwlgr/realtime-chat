ba9986e16243f6c8c35d013f66fe1c1cd2cc9173
Paweł Waligóra <pawelwaligora1337@gmail.com>
2020-05-07 21:07:13 +0200

    add emit event on message sent from frontend to the backend

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const socketio = require('socket.io');
 const http = require('http');
 const router = require('./router');
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
+
 const PORT = process.env.PORT || 5000;
 
 const app = express();
@@ -10,9 +12,27 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on('connection', (socket) => {
-	console.log('socket connection');
+	socket.on('join', ({ name, room }, callback) => {
+		const { error, user } = addUser({ id: socket.id, name, room });
 
-	socket.on('join', ({ name, room }, callback) => {});
+		if (error) return callback(error);
+
+		socket.emit('message', { user: 'admin', text: `${user.name} welcome to the ${user.room}` }); //message to the user
+
+		socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined` }); //message to the room
+
+		socket.join(user.room);
+
+		callback();
+	});
+
+	socket.on('sendMessage', (message, callback) => {
+		const user = getUser(socket.id);
+
+		io.to(user.room).emit('message', { user: user.name, text: message });
+
+		callback();
+	});
 
 	socket.on('disconnect', () => {
 		console.log('socket disconnected');

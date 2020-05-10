11647281a5ec3836c234ae93b0dafd2601f1b45b
Paweł Waligóra <pawelwaligora1337@gmail.com>
2020-05-10 19:55:19 +0200

    add cors

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
+const cors = require('cors');
 const router = require('./router');
 
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
@@ -24,6 +25,8 @@ io.on('connection', (socket) => {
 
 		socket.join(user.room);
 
+		io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+
 		callback();
 	});
 
@@ -31,15 +34,21 @@ io.on('connection', (socket) => {
 		const user = getUser(socket.id);
 
 		io.to(user.room).emit('message', { user: user.name, text: message });
+		io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) }); //upating state when user leaves
 
 		callback();
 	});
 
 	socket.on('disconnect', () => {
-		console.log('socket disconnected');
+		const user = removeUser(socket.id);
+
+		if (user) {
+			io.to(user.room).emit('message', { user: 'admin', text: `${user.name} has left` });
+		}
 	});
 });
 
 app.use(router);
+app.use(cors());
 
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
